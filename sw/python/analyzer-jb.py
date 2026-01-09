de89825543678c88b556e144a7c85ef9e4f5680e
brucemack <bruce@mackinnon.com>
2026-01-09 15:08:37 -0500

    Added margin on Jitter Buffer

--- sw/python/analyzer-jb.py
+++ sw/python/analyzer-jb.py
@@ -1,4 +1,4 @@
-filename = "capture.txt"
+filename = "ecr_qso_1.txt"
 
 buckets = { }
 low_bucket = 0
@@ -16,8 +16,8 @@ with open(filename, 'r') as file:
         # Make timestamp a number
         clean_tokens[0] = int(clean_tokens[0])
         ms = clean_tokens[0] / 1000
-        bucket = int(ms / 4)
-        bucket = bucket * 4000
+        bucket = int(ms / 5)
+        bucket = bucket * 5000
         if not bucket in buckets:
             buckets[bucket] = [ ]
         buckets[bucket].append(clean_tokens)
@@ -35,16 +35,23 @@ for key, value in buckets.items():
 #print(low_bucket, high_bucket)
 #print(sorted_buckets)
 
+#quit()
+
 # Iterate across the time period and create display
 step = 5000
 last_origin_ms = 0
 first_delta = 0
+
+# 1767975601956000
 
 for bucket in range(low_bucket, high_bucket + step, step):
     # Build a list of what was received 
     rx_list = []
     sequence_fault = False
     delta = 0
+    unkey = False
+    played = None
+    interpolate = False
     if bucket in sorted_buckets:            
         for event in sorted_buckets[bucket]:
             if event[1] == "RXV":
@@ -55,9 +62,31 @@ for bucket in range(low_bucket, high_bucket + step, step):
                     sequence_fault = True
                 last_origin_ms = origin_ms
                 if first_delta == 0:
-                    first_delta = event[0] - origin_ms
-                delta = (event[0] - origin_ms) - first_delta
+                    first_delta = event[0] - origin_ms * 1000
+                delta = first_delta - (event[0] - origin_ms * 1000)
+            elif event[1] == "UNK":
+                unkey = True
+            elif event[1] == "POV":
+                played = event[2]
+            elif event[1] == "POI":
+                interpolate = True
     rx_list_str = ""
     for rx in rx_list:
         rx_list_str += " " + str(rx)
-    print(bucket, "\t", rx_list_str, "\t", sequence_fault, "\t", delta)
+    sequence_fault_str = ""
+    if sequence_fault:
+        sequence_fault_str = "*"
+    unkey_str = ""
+    if unkey:
+        unkey_str = "U"
+    delta_str = ""
+    if delta:
+        delta_str = str(int(delta / 1000))
+    played_str = ""
+    if played:
+        played_str = str(played)
+    if interpolate:
+        played_str = "I"
+    print(bucket, "\t", rx_list_str, "\t", sequence_fault_str, "\t", unkey_str, "\t", delta_str, "\t", played_str)
+    if bucket % 20000 == 0:
+        print("-----------------------------------------------------------------------")
