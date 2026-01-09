1431e4d191e2eb91e973c8fa1c2c7f35a9401b92
Bruce MacKinnon <bruce@mackinnon.com>
2026-01-09 10:35:35 -0500

    Analyzer

--- sw/python/analyzer-jb.py
+++ sw/python/analyzer-jb.py
@@ -38,17 +38,26 @@ for key, value in buckets.items():
 # Iterate across the time period and create display
 step = 5000
 last_origin_ms = 0
+first_delta = 0
+
 for bucket in range(low_bucket, high_bucket + step, step):
     # Build a list of what was received 
     rx_list = []
     sequence_fault = False
+    delta = 0
     if bucket in sorted_buckets:            
         for event in sorted_buckets[bucket]:
             if event[1] == "RXV":
                 origin_ms = int(event[2])
                 rx_list.append(origin_ms)
-                # Look for ordering problems/gaps
+                # Look for origin ordering problems/gaps
                 if last_origin_ms != 0 and (origin_ms < last_origin_ms or origin_ms > last_origin_ms + 25):
                     sequence_fault = True
                 last_origin_ms = origin_ms
-    print(bucket, rx_list, sequence_fault)
+                if first_delta == 0:
+                    first_delta = event[0] - origin_ms
+                delta = (event[0] - origin_ms) - first_delta
+    rx_list_str = ""
+    for rx in rx_list:
+        rx_list_str += " " + str(rx)
+    print(bucket, "\t", rx_list_str, "\t", sequence_fault, "\t", delta)
