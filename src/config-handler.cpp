60865af7f1e7224f3844d52c4837d5d8c036ab8f
brucemack <bruce@mackinnon.com>
2026-03-02 20:30:31 -0500

    Working on the echo feature for Patrick N2DYI

--- src/config-handler.cpp
+++ src/config-handler.cpp
@@ -137,7 +137,8 @@ int configHandler(Log& log, const json& cfg, WebUi& webUi, LineIAX2& iax2Channel
                     throw invalid_argument("aslRxMixerSet is missing/invalid");
                 int rxMixerSet = std::stoi(cfg["aslRxMixerSet"].get<std::string>());
 
-                rc = radio2.open(alsaCard, txMixASet, txMixBSet, rxMixerSet);
+                // #### TODO: MAKE ECHO CONFIGURABLE
+                rc = radio2.open(alsaCard, txMixASet, txMixBSet, rxMixerSet, false);
                 if (rc < 0) {
                     if (rc == -12)
                         log.error("Unable to open sound device, busy");
