d2df73fdbecb9e2b43a51ac3b50efdd098d6f0f9
brucemack <bruce@mackinnon.com>
2026-05-16 11:24:56 -0400

    Moved version number, preparing for release build

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ using namespace std;
 using namespace kc1fsz;
 
 // ### TODO: FIGURE OUT HOW TO MAKE THIS AUTOMATIC
-static const char* VERSION = "20260515.0";
+static const char* VERSION = "20260516.0";
 static const char* const GIT_HASH = "?";
 static const char* PUBLIC_USER = "radio";
 
