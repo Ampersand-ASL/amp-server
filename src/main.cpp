db5b44310461471d512a059a32f6c0b1c3445202
brucemack <bruce@mackinnon.com>
2026-05-17 07:26:30 -0400

    Per suggestion from David NR9V, scaling back the rate at which we call snd_pcm_writei() to avoid the possibility of livelock situations. This is an attempt to address the overrun problem that has been observed on the Uno Q.

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ using namespace std;
 using namespace kc1fsz;
 
 // ### TODO: FIGURE OUT HOW TO MAKE THIS AUTOMATIC
-static const char* VERSION = "20260516.1";
+static const char* VERSION = "20260517.0";
 static const char* const GIT_HASH = "?";
 static const char* PUBLIC_USER = "radio";
 
